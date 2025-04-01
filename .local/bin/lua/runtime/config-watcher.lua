b63ba2e789ad91790a52b0338c97edc67ec694a7
elde-n <elden_@tutanota.com>
2025-04-02 02:15:23 +0300

    update watcher

--- .local/bin/lua/runtime/config-watcher.lua
+++ .local/bin/lua/runtime/config-watcher.lua
@@ -5,10 +5,15 @@ local watch_file = require "modules/watch-file"
 local CONFIG_DIRECTORY = os.getenv("HOME") .. "/.config/"
 
 function M.start()
-	return watch_file(CONFIG_DIRECTORY .. "waybar", true, function()
-		-- we have a script that auto ressurects waybar
-		os.execute("killall waybar")
-	end, 0.1)
+	return {
+		watch_file(CONFIG_DIRECTORY .. "waybar", true, function()
+			os.execute("killall waybar")
+		end, 0.1),
+
+		watch_file(CONFIG_DIRECTORY .. "swaync", true, function()
+			os.execute("swaync-client --reload-config")
+		end, 0.1)
+	}
 end
 
 return M
